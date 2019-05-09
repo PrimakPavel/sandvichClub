ad6870ce3307adf81e211267530433c94b5244bd
Pavel Prymak <ppastate@gmail.com>
2019-05-09 16:22:54 +0300

    Task has done by Prymak Pavel

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,11 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -13,18 +17,33 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
+    public static final String LIST_ELEMENTS_SEPARATOR = ", ";
     private static final int DEFAULT_POSITION = -1;
+    private TextView mAlsoKnownAsTv;
+    private TextView mAlsoKnownAsLabelTv;
+    private TextView mIngredientsTv;
+    private TextView mIngredientsLabelTv;
+    private TextView mPlaceOfOriginTv;
+    private TextView mPlaceOfOriginLabelTv;
+    private TextView mDescriptionTv;
+    private TextView mDescriptionLabelTv;
+
+    private ProgressBar mProgressBar;
+    private ImageView mImageLoadErrorIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ImageView posterIv = findViewById(R.id.image_iv);
+
+        findViewsById();
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
+            return;
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
@@ -43,12 +62,38 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
+        populateUI(sandwich);
+        Picasso.get()
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(posterIv, new com.squareup.picasso.Callback() {
+                    @Override
+                    public void onSuccess() {
+                        mProgressBar.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        mImageLoadErrorIv.setVisibility(View.VISIBLE);
+                        mProgressBar.setVisibility(View.GONE);
+                    }
+                });
 
         setTitle(sandwich.getMainName());
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void closeOnError() {
@@ -56,7 +101,59 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void findViewsById() {
+        mAlsoKnownAsTv = findViewById(R.id.also_known_tv);
+        mAlsoKnownAsLabelTv = findViewById(R.id.also_known_label_tv);
+
+        mIngredientsTv = findViewById(R.id.ingredients_tv);
+        mIngredientsLabelTv = findViewById(R.id.detail_ingredients_label_tv);
+
+        mPlaceOfOriginTv = findViewById(R.id.origin_tv);
+        mPlaceOfOriginLabelTv = findViewById(R.id.detail_place_of_origin_label_tv);
+
+        mDescriptionTv = findViewById(R.id.description_tv);
+        mDescriptionLabelTv = findViewById(R.id.detail_description_label_tv);
+
+        mProgressBar = findViewById(R.id.progress_bar);
+        mImageLoadErrorIv = findViewById(R.id.image_load_error_iv);
+
+    }
+
+    private void populateUI(Sandwich sandwich) {
+        //"Also known as" ROW
+        if (!sandwich.getAlsoKnownAs().isEmpty()) {
+            mAlsoKnownAsTv.setVisibility(View.VISIBLE);
+            mAlsoKnownAsLabelTv.setVisibility(View.VISIBLE);
+            for (int i = 0; i < sandwich.getAlsoKnownAs().size(); i++) {
+                mAlsoKnownAsTv.append(sandwich.getAlsoKnownAs().get(i));
+                if (i < sandwich.getAlsoKnownAs().size() - 1) {
+                    mAlsoKnownAsTv.append(LIST_ELEMENTS_SEPARATOR);
+                }
+            }
+        }
+        //"Ingredients" ROW
+        if (!sandwich.getIngredients().isEmpty()) {
+            mIngredientsTv.setVisibility(View.VISIBLE);
+            mIngredientsLabelTv.setVisibility(View.VISIBLE);
+            for (int i = 0; i < sandwich.getIngredients().size(); i++) {
+                mIngredientsTv.append(sandwich.getIngredients().get(i));
+                if (i < sandwich.getIngredients().size() - 1) {
+                    mIngredientsTv.append(LIST_ELEMENTS_SEPARATOR);
+                }
+            }
+        }
+        //"Place of origin" ROW
+        if (!sandwich.getPlaceOfOrigin().isEmpty()) {
+            mPlaceOfOriginTv.setVisibility(View.VISIBLE);
+            mPlaceOfOriginLabelTv.setVisibility(View.VISIBLE);
+            mPlaceOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+        }
+        //"Description" ROW
+        if (!sandwich.getDescription().isEmpty()) {
+            mDescriptionTv.setVisibility(View.VISIBLE);
+            mDescriptionLabelTv.setVisibility(View.VISIBLE);
+            mDescriptionTv.setText(sandwich.getDescription());
+        }
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,85 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    private static final String NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String ALSO_KNOWN_AS = "alsoKnownAs";
+
+    private static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String DESCRIPTION = "description";
+    private static final String IMAGE = "image";
+    private static final String INGREDIENTS = "ingredients";
+
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject sandwichJsonObject = new JSONObject(json);
+
+            if (sandwichJsonObject.has(NAME)) {
+                JSONObject nameJsonObject = sandwichJsonObject.getJSONObject(NAME);
+                if (nameJsonObject.has(MAIN_NAME)) {
+                    sandwich.setMainName(nameJsonObject.getString(MAIN_NAME));
+                }
+                if (nameJsonObject.has(ALSO_KNOWN_AS)) {
+                    JSONArray alsoKnownAsJsonArray = nameJsonObject.getJSONArray(ALSO_KNOWN_AS);
+                    sandwich.setAlsoKnownAs(convertJsonArrayToStringList(alsoKnownAsJsonArray));
+                }
+            }
+
+            if (sandwichJsonObject.has(PLACE_OF_ORIGIN)) {
+                sandwich.setPlaceOfOrigin(sandwichJsonObject.getString(PLACE_OF_ORIGIN));
+            }
+            if (sandwichJsonObject.has(DESCRIPTION)) {
+                sandwich.setDescription(sandwichJsonObject.getString(DESCRIPTION));
+            }
+            if (sandwichJsonObject.has(IMAGE)) {
+                sandwich.setImage(sandwichJsonObject.getString(IMAGE));
+            }
+            if (sandwichJsonObject.has(INGREDIENTS)) {
+                JSONArray ingredientsJsonArray = sandwichJsonObject.getJSONArray(INGREDIENTS);
+                sandwich.setIngredients(convertJsonArrayToStringList(ingredientsJsonArray));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
     }
+
+    private static List<String> convertJsonArrayToStringList(JSONArray jsonArray) {
+        ArrayList<String> list = new ArrayList<>();
+        if (jsonArray != null) {
+            int len = jsonArray.length();
+            for (int i = 0; i < len; i++) {
+                try {
+                    String currentValue = jsonArray.getString(i);
+                    if (currentValue != null) {
+                        list.add(currentValue);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return list;
+    }
+
+    /*{"name":
+        {"mainName":"Ham and cheese sandwich","alsoKnownAs":["Some info about"]},
+        "placeOfOrigin":"",
+            "description":"A ham and cheese sandwich is a common type of sandwich. It is made by putting cheese and sliced ham between two slices of bread. The bread is sometimes buttered and/or toasted. Vegetables            like lettuce, tomato, onion or pickle slices can also be included. Various kinds of mustard and mayonnaise are also common.",
+            "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG",
+            "ingredients":["Sliced bread","Cheese","Ham"]
+    }*/
 }
